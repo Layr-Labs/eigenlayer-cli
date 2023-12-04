0e8bf2221313ba3a5a081e4d42c51190e4877d37
Madhur Shrimal <madhur.shrimal@gmail.com>
2023-12-04 13:59:32 -0800

    binary release

---
+++ scripts/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Run with ./scripts/build.sh <optional_version>
+if ! [[ "$0" =~ scripts/build.sh ]]; then
+  echo "must be run from repository root"
+  exit 1
+fi
+
+go build -v -o bin/eigenlayer
