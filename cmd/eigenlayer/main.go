21d976adee20a573593b2913c404f1dcd620b9c6
Madhur Shrimal <madhur.shrimal@gmail.com>
2024-04-08 13:47:29 -0700

    bump version (#101)

--- cmd/eigenlayer/main.go
+++ cmd/eigenlayer/main.go
@@ -23,7 +23,7 @@ func main() {
 
 	app.Name = "eigenlayer"
 	app.Usage = "EigenLayer CLI"
-	app.Version = "0.7.1"
+	app.Version = "0.7.2"
 	app.Copyright = "(c) 2024 EigenLabs"
 
 	// Initialize the dependencies
