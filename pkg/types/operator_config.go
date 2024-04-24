bc2e20082677a3b208211c16ac2f39052460d871
Madhur Shrimal <madhur.shrimal@gmail.com>
2024-04-24 16:26:46 -0700

    remove old config

--- pkg/types/operator_config.go
+++ pkg/types/operator_config.go
@@ -13,39 +13,6 @@ const (
 	LocalKeystoreSigner SignerType = "local_keystore"
 )
 
-type OperatorConfig struct {
-	Operator                      eigensdkTypes.Operator `yaml:"operator"`
-	ELSlasherAddress              string                 `yaml:"el_slasher_address"`
-	BlsPublicKeyCompendiumAddress string                 `yaml:"bls_public_key_compendium_address"`
-	EthRPCUrl                     string                 `yaml:"eth_rpc_url"`
-	PrivateKeyStorePath           string                 `yaml:"private_key_store_path"`
-	SignerType                    SignerType             `yaml:"signer_type"`
-	BlsPrivateKeyStorePath        string                 `yaml:"bls_private_key_store_path"`
-	ChainId                       big.Int                `yaml:"chain_id"`
-}
-
-func (config OperatorConfig) MarshalYAML() (interface{}, error) {
-	return struct {
-		Operator                      eigensdkTypes.Operator `yaml:"operator"`
-		ELSlasherAddress              string                 `yaml:"el_slasher_address"`
-		BlsPublicKeyCompendiumAddress string                 `yaml:"bls_public_key_compendium_address"`
-		EthRPCUrl                     string                 `yaml:"eth_rpc_url"`
-		PrivateKeyStorePath           string                 `yaml:"private_key_store_path"`
-		SignerType                    SignerType             `yaml:"signer_type"`
-		BlsPrivateKeyStorePath        string                 `yaml:"bls_private_key_store_path"`
-		ChainID                       int64                  `yaml:"chain_id"`
-	}{
-		Operator:                      config.Operator,
-		ELSlasherAddress:              config.ELSlasherAddress,
-		BlsPublicKeyCompendiumAddress: config.BlsPublicKeyCompendiumAddress,
-		EthRPCUrl:                     config.EthRPCUrl,
-		PrivateKeyStorePath:           config.PrivateKeyStorePath,
-		SignerType:                    config.SignerType,
-		BlsPrivateKeyStorePath:        config.BlsPrivateKeyStorePath,
-		ChainID:                       config.ChainId.Int64(),
-	}, nil
-}
-
 type OperatorConfigNew struct {
 	Operator                   eigensdkTypes.Operator `yaml:"operator"`
 	ELDelegationManagerAddress string                 `yaml:"el_delegation_manager_address"`
