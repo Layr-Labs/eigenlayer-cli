86236245058834bb2a68969d39cc4d26191ac088
FromTheRain <bdchatham@gmail.com>
2024-12-10 14:49:33 -0800

    Adding `appointee` `remove` and `set` command implementation and unit tests. (#250)
    Co-authored-by: Brandon Chatham <brandon.chatham@eigenlabs.org>

--- pkg/user/appointee/set.go
+++ pkg/user/appointee/set.go
@@ -1,14 +1,31 @@
 package appointee
 
 import (
+	"context"
 	"sort"
 
+	"github.com/Layr-Labs/eigenlayer-cli/pkg/internal/common"
 	"github.com/Layr-Labs/eigenlayer-cli/pkg/internal/common/flags"
 	"github.com/Layr-Labs/eigenlayer-cli/pkg/telemetry"
+	"github.com/Layr-Labs/eigenlayer-cli/pkg/utils"
+	"github.com/Layr-Labs/eigensdk-go/chainio/clients/elcontracts"
+	"github.com/Layr-Labs/eigensdk-go/logging"
+	eigenSdkUtils "github.com/Layr-Labs/eigensdk-go/utils"
+	gethcommon "github.com/ethereum/go-ethereum/common"
+	gethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+
 	"github.com/urfave/cli/v2"
 )
 
-func SetCmd() *cli.Command {
+type SetUserPermissionWriter interface {
+	SetPermission(
+		ctx context.Context,
+		request elcontracts.SetPermissionRequest,
+	) (*gethtypes.Receipt, error)
+}
+
+func SetCmd(generator func(logging.Logger, *setConfig) (SetUserPermissionWriter, error)) *cli.Command {
 	setCmd := &cli.Command{
 		Name:      "set",
 		Usage:     "user appointee set --account-address <AccountAddress> --appointee-address <AppointeeAddress> --target-address <TargetAddress> --selector <Selector>",
@@ -16,11 +33,129 @@ func SetCmd() *cli.Command {
 		Description: `
 		Grant a user a permission.'.
 		`,
+		Action: func(c *cli.Context) error {
+			return setUserPermission(c, generator)
+		},
 		After: telemetry.AfterRunAction(),
 		Flags: setCommandFlags(),
 	}
 
 	return setCmd
+}
+
+func setUserPermission(
+	cliCtx *cli.Context,
+	generator func(logging.Logger, *setConfig) (SetUserPermissionWriter, error),
+) error {
+	ctx := cliCtx.Context
+	logger := common.GetLogger(cliCtx)
+
+	config, err := readAndValidateSetConfig(cliCtx, logger)
+	if err != nil {
+		return eigenSdkUtils.WrapError("failed to read and validate user can call config", err)
+	}
+	cliCtx.App.Metadata["network"] = config.ChainID.String()
+	permissionWriter, err := generator(logger, config)
+	if err != nil {
+		return err
+	}
+	receipt, err := permissionWriter.SetPermission(
+		ctx,
+		elcontracts.SetPermissionRequest{
+			Account:        config.AccountAddress,
+			UserAddress:    config.UserAddress,
+			Target:         config.Target,
+			Selector:       config.Selector,
+			WaitForReceipt: true,
+		},
+	)
+	if err != nil {
+		return err
+	}
+	common.PrintTransactionInfo(receipt.TxHash.String(), config.ChainID)
+	return nil
+}
+
+func generateSetUserPermissionWriter(
+	prompter utils.Prompter,
+) func(
+	logger logging.Logger,
+	config *setConfig,
+) (SetUserPermissionWriter, error) {
+	return func(logger logging.Logger, config *setConfig) (SetUserPermissionWriter, error) {
+		ethClient, err := ethclient.Dial(config.RPCUrl)
+		if err != nil {
+			return nil, eigenSdkUtils.WrapError("failed to create new eth client", err)
+		}
+		elWriter, err := common.GetELWriter(
+			config.AccountAddress,
+			&config.SignerConfig,
+			ethClient,
+			elcontracts.Config{
+				PermissionsControllerAddress: config.PermissionManagerAddress,
+			},
+			prompter,
+			config.ChainID,
+			logger,
+		)
+		return elWriter, err
+	}
+}
+
+func readAndValidateSetConfig(cliContext *cli.Context, logger logging.Logger) (*setConfig, error) {
+	accountAddress := gethcommon.HexToAddress(cliContext.String(AccountAddressFlag.Name))
+	userAddress := gethcommon.HexToAddress(cliContext.String(AppointeeAddressFlag.Name))
+	ethRpcUrl := cliContext.String(flags.ETHRpcUrlFlag.Name)
+	network := cliContext.String(flags.NetworkFlag.Name)
+	environment := cliContext.String(flags.EnvironmentFlag.Name)
+	target := gethcommon.HexToAddress(cliContext.String(TargetAddressFlag.Name))
+	selector := cliContext.String(SelectorFlag.Name)
+	selectorBytes, err := common.ValidateAndConvertSelectorString(selector)
+	if err != nil {
+		return nil, err
+	}
+	signerConfig, err := common.GetSignerConfig(cliContext, logger)
+	if err != nil {
+		// We don't want to throw error since people can still use it to generate the claim
+		// without broadcasting it
+		logger.Debugf("Failed to get signer config: %s", err)
+	}
+
+	if environment == "" {
+		environment = common.GetEnvFromNetwork(network)
+	}
+
+	chainID := utils.NetworkNameToChainId(network)
+	cliContext.App.Metadata["network"] = chainID.String()
+	permissionManagerAddress := cliContext.String(PermissionControllerAddressFlag.Name)
+
+	if common.IsEmptyString(permissionManagerAddress) {
+		permissionManagerAddress, err = common.GetPermissionManagerAddress(utils.NetworkNameToChainId(network))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	logger.Debugf(
+		"Env: %s, network: %s, chain ID: %s, PermissionManager address: %s",
+		environment,
+		network,
+		chainID,
+		permissionManagerAddress,
+	)
+
+	return &setConfig{
+		Network:                  network,
+		RPCUrl:                   ethRpcUrl,
+		AccountAddress:           accountAddress,
+		UserAddress:              userAddress,
+		Target:                   target,
+		Selector:                 selectorBytes,
+		SignerConfig:             *signerConfig,
+		PermissionManagerAddress: gethcommon.HexToAddress(permissionManagerAddress),
+		ChainID:                  chainID,
+		Environment:              environment,
+	}, nil
 }
 
 func setCommandFlags() []cli.Flag {
@@ -30,6 +165,9 @@ func setCommandFlags() []cli.Flag {
 		&AppointeeAddressFlag,
 		&TargetAddressFlag,
 		&SelectorFlag,
+		&flags.NetworkFlag,
+		&flags.EnvironmentFlag,
+		&flags.ETHRpcUrlFlag,
 	}
 	sort.Sort(cli.FlagsByName(cmdFlags))
 	return append(cmdFlags, flags.GetSignerFlags()...)
