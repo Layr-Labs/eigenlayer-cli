f8d0a42d774d1027c12eb41db7a9ee1119e46217
Brandon Chatham <brandon.chatham@eigenlabs.org>
2024-12-20 15:28:59 -0800

    Updating the bats test runner and environment binary filepath.

--- tests/user/user-integration-tests-runner.sh
+++ tests/user/user-integration-tests-runner.sh
@@ -1,9 +1,29 @@
+#!/bin/bash
+
 echo "Starting User command integration tests."
 
 export $(cat .env | xargs)
 
-bats "admin/admin_lifecycle_tests.sh"
-bats "appointee/appointee_lifecycle_tests.sh"
-bats "appointee/appointee_output_tests.sh"
+any_test_failures=0
+
+run_bats_test() {
+  local test_file=$1
+  bats "$test_file"
+  if [ $? -ne 0 ]; then
+    any_test_failures=1
+  fi
+}
+
+run_bats_test "admin/admin_lifecycle_tests.sh"
+run_bats_test "appointee/appointee_lifecycle_tests.sh"
+run_bats_test "appointee/appointee_output_tests.sh"
 
 rm -rf output/
+
+if [ $any_test_failures -eq 0 ]; then
+  echo "All tests passed."
+else
+  echo "Some tests failed."
+fi
+
+exit $any_test_failures
