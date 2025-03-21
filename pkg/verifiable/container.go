b5fcf361679869f99698eb4d6c650340befee271
Brandon Chatham <brandon.chatham@eigenlabs.org>
2025-03-21 14:28:10 -0700

    Adding verifiable container commands to sign releases and verify signatures using annotations.

---
+++ pkg/verifiable/container.go
@@ -0,0 +1,21 @@
+package verifiable
+
+import (
+	"github.com/Layr-Labs/eigenlayer-cli/pkg/internal/registry"
+	"github.com/Layr-Labs/eigenlayer-cli/pkg/utils"
+	"github.com/Layr-Labs/eigenlayer-cli/pkg/verifiable/container"
+	"github.com/urfave/cli/v2"
+)
+
+func NewContainerCmd(prompter utils.Prompter) *cli.Command {
+	var containerCmd = &cli.Command{
+		Name:  "container",
+		Usage: "Manage operations related to container verification.",
+		Subcommands: []*cli.Command{
+			container.NewSignContainerCmd(prompter, registry.NewGithubContainerRegistry()),
+			container.NewVerifyContainerCmd(),
+		},
+	}
+
+	return containerCmd
+}
