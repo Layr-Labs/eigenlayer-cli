9334975618cd0b6e77a61e8df8df94773a2161f5
Sean McGary <sean@seanmcgary.com>
2025-05-01 08:56:11 -0500

    fix: PrettyPrintStruct was panicing due to incorrect reflect parsing

--- pkg/internal/common/print.go
+++ pkg/internal/common/print.go
@@ -7,6 +7,18 @@ import (
 
 func PrettyPrintStruct(data interface{}) {
 	v := reflect.ValueOf(data)
+
+	// If it's a pointer, get the underlying element
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+
+	// Check if it's a struct
+	if v.Kind() != reflect.Struct {
+		fmt.Println("Not a struct or pointer to struct")
+		return
+	}
+
 	typeOfS := v.Type()
 
 	for i := 0; i < v.NumField(); i++ {
